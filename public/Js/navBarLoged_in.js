0c19a3835a68da8c1e3036fc36f793b498cdf6a3
KingRichardpt45 <ricfrancvi@gmail.com>
2023-04-30 13:46:36 +0100

    comit Tipo de letra corrigido

--- public/Js/navBarLoged_in.js
+++ public/Js/navBarLoged_in.js
@@ -3,6 +3,14 @@ let navLogedIn = document.getElementById("navBarLoged_in");
 let menuButton = document.getElementById("navLink_Menu");
 let click = false;
 
+switch(window.location.pathname){
+    case "/":
+        document.getElementById("navLink_Home_content").classList.replace("ButtonBlack","ButtonBlackActive");
+        break;
+
+    /*reste*/
+}
+
 // Adicionando um EventListener para quando clicarmos no menu.
 menuButton.addEventListener("click", (event) => {
     //click faz com que o butão seja toogle
@@ -10,15 +18,15 @@ menuButton.addEventListener("click", (event) => {
     if (click){
         menuButton.style.backgroundColor="#70f5ff";
         navLogedIn.insertAdjacentHTML("afterend",`
-        <div id="MenuCascata">
+        <div id="MenuCascata" class="ButtonBlack">
             <a class="MenuCascataButton ButtonBlack" id="navLink_Home" href="/" >About Us</a>
             <a class="MenuCascataButton ButtonBlack" id="navLink_Home" href="/" >Profile</a>
             <a class="MenuCascataButton ButtonBlack" id="navLink_Home" href="/" >Play</a>
         </div>
-        `)
+        `);
     }else{
-        menuButton.style.backgroundColor="#000000";
-        let menu = document.getElementById("MenuCascata")
+        menuButton.style="none";
+        let menu = document.getElementById("MenuCascata");
         if(menu != null)
             menu.remove();
     }
