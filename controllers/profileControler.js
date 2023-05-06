ce3dd50a9fcad668ceb8dc62cc0ade0d3728d7f5
KingRichardpt45 <ricfrancvi@gmail.com>
2023-05-06 16:03:46 +0100

    fix

---
+++ controllers/profileControler.js
@@ -0,0 +1,7 @@
+// creating the profile handler
+const profileControler = function profileControler( req , res) {
+    res.render("profile");
+}
+
+// exporting the handler
+module.exports = profileControler;
