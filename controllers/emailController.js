07ad1fa52d5bb2de7c51333248957d935d760f70
jpedro85 <pedro.games30@gmail.com>
2023-05-21 16:36:39 +0100

    fix

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -40,11 +40,6 @@ const sendConfirmEmail = async function (destinationEmail, emailCode) {
             from: SMTP_CONFIG.user,
             to: destinationEmail,
             subject: "Please confirm your account",
-            // html: `<h1>Email Confirmation</h1>
-            //       <h2>Hello</h2>
-            //       <p>Thank you for registering. Please confirm your email by clicking on the following link</p>
-            //       <a href=http://localhost:3000/confirm/${emailCode}> Click here</a>
-            //       </div>`,
             html: `<!DOCTYPE html><html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en"><head><title></title><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]--><!--[if !mso]><!--><link 
       href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700;900&amp;display=swap" rel="stylesheet" type="text/css"><!--<![endif]--><style>
       *{box-sizing:border-box}body{margin:0;padding:0}a[x-apple-data-detectors]{color:inherit!important;text-decoration:inherit!important}#MessageViewBody a{color:inherit;text-decoration:none}p{line-height:inherit}.desktop_hide,.desktop_hide table{mso-hide:all;display:none;max-height:0;overflow:hidden}.image_block img+div{display:none} @media (max-width:720px){.social_block.desktop_hide .social-table{display:inline-block!important}.image_block img.big,.row-content{width:100%!important}.mobile_hide{display:none}.stack .column{width:100%;display:block}.mobile_hide{min-height:0;max-height:0;max-width:0;overflow:hidden;font-size:0}.desktop_hide,.desktop_hide table{display:table!important;max-height:none!important}}
