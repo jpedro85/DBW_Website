21436943998f4d52d482e2a51a08bf4736a61b15
jpedro85 <104324234+jpedro85@users.noreply.github.com>
2023-05-22 17:16:14 +0100

    Delete smtp.config.js

--- config/smtp.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    host:"smtp.gmail.com",
-    port : 587,
-    secret: "your-secret-key",
-    user: "khajiit.business@gmail.com",
-    pass: "ldsxccmjdmfyzvbb",
-    pool: true,
-}
